416e436d2dc30f2f11350773c0c233383b836c91
Simon Larsen <simonhffh@gmail.com>
2016-02-05 11:49:05 +0100

    Added simple exe for compilation

---
+++ src/test/test.cpp
@@ -0,0 +1,10 @@
+#include <setops/binary_find.hpp>
+#include <setops/difference.hpp>
+#include <setops/intersection.hpp>
+#include <setops/merge.hpp>
+#include <setops/symmetric_difference.hpp>
+#include <setops/union.hpp>
+
+int main(int argc, const char *argv[]) {
+	return 0;
+}
